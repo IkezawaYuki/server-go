be218ada9e7600bf2e4c109d153a0f8ea48464a5
yikezawa <y.ikezawa93@gmail.com>
2020-08-29 23:55:54 +0900

    mod

--- main.go
+++ main.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"fmt"
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/google"
+	"github.com/stretchr/objx"
 	"net/http"
+	"os"
 )
 
 func main(){
+
+	_ = godotenv.Load()
+
+	gomniauth.SetSecurityKey(os.Getenv("SECURITY_KEY"))
+	gomniauth.WithProviders(
+		google.New(
+			os.Getenv("CLIENT_ID"),
+			os.Getenv("SECRET_VALUE"),
+			os.Getenv("REDIRECT_URL"),
+			),
+		)
+
 	// Echo instance
 	e := echo.New()
 
@@ -16,6 +34,7 @@ func main(){
 
 	// Routes
 	e.GET("/", hello)
+	e.GET("/clear", clear)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":8080"))
@@ -27,9 +46,37 @@ func hello(c echo.Context) error {
 
 
 func login(c echo.Context) error{
-
+	provider, err := gomniauth.Provider("google")
+	if err != nil{
+		panic(err)
+	}
+	loginURL, err := provider.GetBeginAuthURL(nil, nil)
+	if err != nil{
+		panic(err)
+	}
+	return c.Redirect(302, loginURL)
 }
 
 func callback(c echo.Context) error{
-	
+	provider, err := gomniauth.Provider("google")
+	if err != nil{
+		panic(err)
+	}
+
+	cred, err := provider.CompleteAuth(objx.MustFromURLQuery(c.QueryString()))
+	if err != nil{
+		panic(err)
+	}
+
+	user, err := provider.GetUser(cred)
+	if err != nil{
+		panic(err)
+	}
+
+	fmt.Println(user)
+	return c.Redirect(302, "/clear")
+}
+
+func clear(c echo.Context) error{
+	return c.String(http.StatusOK, "clear!!!!!")
 }
